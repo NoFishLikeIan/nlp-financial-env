a464d43a2df6a4f4999dc191aef8a8b28e92ef15
NoFishLikeIan <andreatitton96@gmail.com>
2020-09-28 18:47:49 +0200

    🪐 url option for pdfs

--- utils/read_pdf.py
+++ utils/read_pdf.py
@@ -1,9 +1,24 @@
 import PyPDF2
 import string
-
+import requests
 import re
+import io
+import os
 
 from .parser_model import nlp
+
+
+def is_url(to_validate):
+
+    url_regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+    return re.match(url_regex, to_validate) is not None
 
 def extract_statements(text):
 
@@ -35,20 +50,38 @@ def extract_statements(text):
 
   return sentences
 
+
+def pdf_to_text(pdf):
+    text = ""
+    n_pages = pdf.getNumPages()
+
+    for i in range(n_pages):
+        page = pdf.getPage(i)
+
+        page_text = page.extractText()
+
+        text += f"{page_text}\n"
+
+    return text
+
+
 def path_to_sentences(filepath: str) -> str:
 
     text = ""
 
-    with open(filepath, "rb") as file:
-        pdf = PyPDF2.PdfFileReader(file)
+    if is_url(filepath):
+        response = requests.get(filepath)
 
-        n_pages = pdf.getNumPages()
+        with io.BytesIO(response.content) as file:
+            pdf = PyPDF2.PdfFileReader(file)
+            text = pdf_to_text(pdf)
 
-        for i in range(n_pages):
-            page = pdf.getPage(i)
+    elif os.path.isfile(filepath):
+        with open(filepath, "rb") as file:
+            pdf = PyPDF2.PdfFileReader(file)
+            text = pdf_to_text(pdf)
 
-            page_text = page.extractText()
-
-            text += f"{page_text}\n"
+    else:
+        raise ValueError("Filepath not understood")
 
     return extract_statements(text)
